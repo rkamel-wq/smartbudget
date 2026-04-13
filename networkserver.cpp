2eb5ef7465aa318305663c08a5d2943a4cf7903b
Rola <rolakamel0@gmail.com>
2026-04-14 01:39:31 +0200

    Add asynchronous TCP server for handling transaction messages

---
+++ networkserver.cpp
@@ -0,0 +1,3 @@
+#include "networkserver.h"
+
+NetworkServer::NetworkServer() {}
---
+++ networkserver.h
@@ -0,0 +1,10 @@
+#ifndef NETWORKSERVER_H
+#define NETWORKSERVER_H
+
+class NetworkServer
+{
+public:
+    NetworkServer();
+};
+
+#endif // NETWORKSERVER_H
