ffe5f90e4c85928c46b343f7bc616f8c0f78251b
Rola <rolakamel0@gmail.com>
2026-04-14 02:02:31 +0200

    Add asynchronous Boost.Asio TCP client for SmartBudget

---
+++ networkclient.cpp
@@ -0,0 +1,3 @@
+#include "networkclient.h"
+
+NetworkClient::NetworkClient() {}
---
+++ networkclient.h
@@ -0,0 +1,10 @@
+#ifndef NETWORKCLIENT_H
+#define NETWORKCLIENT_H
+
+class NetworkClient
+{
+public:
+    NetworkClient();
+};
+
+#endif // NETWORKCLIENT_H
